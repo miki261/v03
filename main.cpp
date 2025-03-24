ef7552666d7cf837f1f07088b3950cca7d86795e
miki261 <matej261matej@gmail.com>
2025-03-24 20:38:53 +0100

    vjezba gotova

--- main.cpp
+++ main.cpp
@@ -1,36 +1,53 @@
-#include <windows.h>
+﻿#include <windows.h>
 #include "nwpdlg.h"
 #include "resource.h"
 
-class main_dialog : public vsite::nwp::dialog 
+class main_dialog : public vsite::nwp::dialog
 {
+public:
+    POINT position{};
+
 protected:
-	int idd() const override
-	{ 
-		return IDD_DIALOG1; 
-	}
-	bool on_init_dialog() override
-	{
-		// TODO: set initial values to edit controls
-		return true;
-	}
-	bool on_ok() override
-	{
-		// TODO: get current values from edit controls
-		// TODO: if not valid return false
-		return true;
-	}
-	void on_cancel() override { }
-	bool on_command(int id, int code) override { return false; }
+    int idd() const override
+    {
+        return IDD_DIALOG1;
+    }
+
+    bool on_init_dialog() override
+    {
+        set_int(IDC_EDIT1, position.x);
+        set_int(IDC_EDIT2, position.y);
+        return true;
+    }
+
+    bool on_ok() override
+    {
+        try
+        {
+            position.x = get_int(IDC_EDIT1);
+            position.y = get_int(IDC_EDIT2);
+        }
+        catch (...)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    void on_cancel() override { }
 };
 
 int WINAPI WinMain(HINSTANCE instance, HINSTANCE, LPSTR, int)
 {
-	main_dialog dlg;
-	// TODO: find current mouse position and transfer to dialog
-	if(dlg.do_modal(instance, 0) == IDOK)
-	{
-		// TODO: set mouse position to coordinates from dialog
-	}
-	return 0;
+    POINT cursorPos;
+    GetCursorPos(&cursorPos);
+
+    main_dialog dlg;
+    dlg.position = cursorPos;
+
+    if (dlg.do_modal(instance, 0) == IDOK)
+    {
+        SetCursorPos(dlg.position.x, dlg.position.y);
+    }
+    return 0;
 }
